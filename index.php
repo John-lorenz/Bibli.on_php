e26fd0f858e2c5e8f4c92e443674fb90a35326ca
Arthur Sá <arthur_sa@estudante.sesisenai.org.br>
2023-04-27 15:22:39 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
 <!DOCTYPE html>
-<!-- Coding by CodingLab || www.codinglabweb.com -->
 <html lang="pt-br">
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <!--<title>Website with Login & Registration Form</title>-->
     <link rel="stylesheet" href="css/style.css"/>
-    <!-- Unicons -->
-    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
   </head>
   <body class="fundo">
-    <!-- Header -->
+    
     <header class="header">
       <nav class="nav">
         <img src="images/logobibli_dark.png" href="#" class="nav_logo" width=200em></img>
@@ -27,13 +23,11 @@
         <button class="button" id="form-open">Login</button>
       </nav>
     </header>
-
-    <!-- Home -->
     <section class="home">
       <div class="form_container">
       
         <i class="uil uil-times form_close"></i>
-        <!-- Login From -->
+   
         <div class="form login_form">
           <form method="post">
             <h2>Login</h2>
@@ -62,7 +56,7 @@
           </form>
         </div>
 
-        <!-- Signup From -->
+  
         <div class="form signup_form">
           <form action="cadastrarBanco.php" method="post">
             <h2>Signup</h2>
@@ -100,7 +94,7 @@ session_start();
 if (isset($_POST['login_email']) && isset($_POST['login_senha'])) {
     
 
-    // Conecta ao banco de dados
+
     $host = '127.0.0.1';
     $usuario = 'root';
     $senha_db = 'root';
@@ -110,12 +104,12 @@ if (isset($_POST['login_email']) && isset($_POST['login_senha'])) {
     $email = $conexao -> real_escape_string($_POST['login_email']);
     $senha = $conexao -> real_escape_string(md5($_POST['login_senha']));
 
-    // Verifica se houve erro na conexão
+
     if ($conexao -> connect_errno) {
         die('Erro ao conectar ao banco de dados: ' . $conexao->connect_errno);
     }
 
-    // Verifica se o usuário e a senha estão corretos
+
     $sql = "SELECT * FROM usuarios WHERE email='$email' AND senha='$senha'";
     $resultado = $conexao-> query($sql);
     $verificaLogin = $resultado -> fetch_assoc();
@@ -128,7 +122,7 @@ if (isset($_POST['login_email']) && isset($_POST['login_senha'])) {
         exit;
     }
 
-    // Fecha a conexão com o banco de dados
+
     $conexao->close();
 }
 ?>
